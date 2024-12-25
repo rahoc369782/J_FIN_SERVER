e7864c1896bcde34d4883807a9fada6320142509
rahoc369782 <rahuldarekar369782@gmail.com>
2024-12-25 06:32:02 +0000

    fix : stg environment require file removed

--- config/global.config.js
+++ config/global.config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 export const CONFIG = {
-  TRANSACTION_FILE_PATH: process.env.J_FINANCE_LEDGER_PATH,,
+  TRANSACTION_FILE_PATH: process.env.J_FINANCE_LEDGER_PATH,
   USER: "cretoprof@gmail.com",
   APP_PASSWORD: "bondutvwjwvhruvu",
   GIT_CREDS: {
